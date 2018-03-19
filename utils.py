48afe2c8e487154e07487cd80dffc877e1838ee4
Krzysztof Nowak <k.nowak@cern.ch>
2018-03-19 13:48:07 +0100

    utils fix
    Signed-off-by: Krzysztof Nowak <k.nowak@cern.ch>

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ def filter_text(text, fn):
     return '\n'.join(line for line in text.split('\n') if not fn(line))
 
 def map_text(text, match_fn, map_fn):
+    pass
 
 def remove_setup_py_pypi_classifier(text):
     def pypy_fn(line):
