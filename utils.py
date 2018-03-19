46fc4346716eea2799a08b1cab002818521c664d
Krzysztof Nowak <k.nowak@cern.ch>
2018-03-19 15:49:51 +0100

    added python dev dependency updaters
    Signed-off-by: Krzysztof Nowak <k.nowak@cern.ch>

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import re
 # Compiled regular expression pattern
 RE_DEV_STATUS = re.compile(r"(.*)('Development Status ::.*')(.*)")
+RE_INVENIO_DEV_DEP = re.compile(r"(.*)(invenio-.*)(1.0.0)(\.?[ab]+[0-9]{1,2})(.*)")
+RE_INVENIO_SEARCH_DEV_DEP = re.compile(r"(invenio_)(.*)( = )(.*)(1.0.0)([ab]+[0-9]{1,2})(.*)")
 
 # Constants
 TROVE_DEV_STATUS = "Development Status :: 5 - Production/Stable"
@@ -16,21 +18,39 @@ def remove_setup_py_pypi_classifier(text):
         return 'Implementation :: PyPy' not in line
     return filter_text(text, pypy_trove)
 
+
 def update_setup_py_development_status(text):
     def dev_status(line):
         m = re.match(RE_DEV_STATUS, line)
         if m:
-            pref, cls, suff = m.groups()
-            return pref + "'{}'".format(TROVE_DEV_STATUS) + suff
-        else:
-            return line
+            return "{0}'{trove}'{2}".format(*m.groups(), trove=TROVE_DEV_STATUS)
+        return line
     return map_text(text, dev_status)
+
+
+def update_setup_py_invenio_dev_deps(text):
+    def dev_dep(line):
+        m = re.match(RE_INVENIO_DEV_DEP, line)
+        if m:
+            grps = m.groups() # (prefix, invenio-*, 1.0.0, {a,b}{0-9}, suffix)
+            return "{0}{1}{2}{4}".format(*m.groups())  # Skip the 'a3' 'b3', etc.
+        return line
+
+    def dev_dep_search(line):
+        m = re.match(RE_INVENIO_SEARCH_DEV_DEP, line)
+        if m:
+            return "{0}{1}{2}{3}{4}{6}".format(*m.groups())  # Skip the 'a3' 'b3', 'a13' etc.
+        return line
+    text = map_text(text, dev_dep)
+    text = map_text(text, dev_dep_search)
+    return text
 
 
 def update_setup_py_pypi_classifiers(text):
     functs = [
         remove_setup_py_pypi_classifier,
         update_setup_py_development_status,
+        update_setup_py_invenio_dev_deps,
     ]
     for fn in functs:
         text = fn(text)
